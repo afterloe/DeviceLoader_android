e9a13ed6020906b4dff135fcc12cc8b51fa8b5cc
afterloe <lm6289511@gmail.com>
2019-03-30 20:23:31 +0800

    feat: 初次打包

--- app/src/main/java/com/github/afterloe/pifinder/ui/activity/ConfigActivity.java
+++ app/src/main/java/com/github/afterloe/pifinder/ui/activity/ConfigActivity.java
@@ -1,10 +1,12 @@
 package com.github.afterloe.pifinder.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -38,11 +40,6 @@ public class ConfigActivity extends AppCompatActivity implements Serializable, N
         initData();
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
-    }
-
     private void initView() {
         toolbar = findViewById(R.id.toolbar);
         nav_view = findViewById(R.id.nav_view);
@@ -68,6 +65,64 @@ public class ConfigActivity extends AppCompatActivity implements Serializable, N
         if(version != null) {
             String msg = String.format(ResUtils.getString(R.string.menu_drysister_version), version);
             tv_nav_title.setText(msg);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.nav_see_little_sister:
+//                if (mFgManager.findFragmentByTag(DryConstant.FG_LITTLE_SISTER) == null) {
+//                    mFgManager.beginTransaction().replace(R.id.cly_main_content,
+//                            LittleSisterFragment.newInstance(), DryConstant.FG_LITTLE_SISTER).commit();
+//                    toolbar.setTitle(ResUtils.getString(R.string.menu_see_little_sister));
+//                }
+//                break;
+//            case R.id.nav_see_news:
+//                if (mFgManager.findFragmentByTag(DryConstant.FG_NEWS) == null) {
+//                    mFgManager.beginTransaction().replace(R.id.cly_main_content,
+//                            NewsFragment.newInstance(), DryConstant.FG_NEWS).commit();
+//                    toolbar.setTitle(ResUtils.getString(R.string.menu_see_news));
+//                }
+//                break;
+//            case R.id.nav_use_check_weather:
+//                if (mFgManager.findFragmentByTag(DryConstant.FG_WEATHER) == null) {
+//                    mFgManager.beginTransaction().replace(R.id.cly_main_content,
+//                            WeatherFragment.newInstance(), DryConstant.FG_WEATHER).commit();
+//                    toolbar.setTitle(ResUtils.getString(R.string.menu_use_check_weather));
+//                }
+//                break;
+//            case R.id.nav_use_check_subway:
+//                if (mFgManager.findFragmentByTag(DryConstant.FG_SUBWAY) == null) {
+//                    mFgManager.beginTransaction().replace(R.id.cly_main_content,
+//                            SubwayFragment.newInstance(), DryConstant.FG_SUBWAY).commit();
+//                    toolbar.setTitle(ResUtils.getString(R.string.menu_use_check_subway));
+//                }
+//                break;
+//            case R.id.nav_use_tools:
+//                if (mFgManager.findFragmentByTag(DryConstant.FG_TOOLS) == null) {
+//                    mFgManager.beginTransaction().replace(R.id.cly_main_content,
+//                            ToolsFragment.newInstance(), DryConstant.FG_TOOLS).commit();
+//                    toolbar.setTitle(ResUtils.getString(R.string.menu_use_tools));
+//                }
+//                break;
+//            case R.id.nav_else_setting:
+//                startActivity(new Intent(this, SettingActivity.class));
+//                break;
+//            case R.id.nav_else_about:
+//                startActivity(new Intent(this, AboutActivity.class));
+//                break;
+//        }
+//        drawer_layout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
